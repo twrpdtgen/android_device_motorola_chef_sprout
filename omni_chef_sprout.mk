99955ed7c7a1e3974f31ce27ee450092e350302c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-19 13:54:22 +0200

    chef_sprout: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: motorola Model: motorola one power Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ omni_chef_sprout.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from chef_sprout device
+$(call inherit-product, device/motorola/chef_sprout/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := chef_sprout
+PRODUCT_NAME := omni_chef_sprout
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := motorola one power
+PRODUCT_MANUFACTURER := motorola
